76d4d276b435d95e4ffccf070b8c24d4d557c560
kumarimonii <120780404+kumarimonii@users.noreply.github.com>
2023-05-29 04:06:15 +0530

    inital commit

---
+++ crud/src/envirnonment/environment.ts
@@ -0,0 +1,11 @@
+export const environment={
+    production :false,
+    firebaseConfig :{
+        apiKey: "AIzaSyAOxULDBHRJBlUzK5t9DsKSkrjUFY6tes8",
+        authDomain: "assignment-34428.firebaseapp.com",
+        projectId: "assignment-34428",
+        storageBucket: "assignment-34428.appspot.com",
+        messagingSenderId: "714902858125",
+        appId: "1:714902858125:web:35f2d501d38dcf8b7ff5c4"
+      }
+    }
